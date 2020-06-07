68a9d0ee1c3c6e77415a14813efad1f792c17690
justin <justin@justinbennett.ca>
2020-06-07 02:54:24 -0400

    fixing many things

--- index.js
+++ index.js
@@ -1,34 +1,174 @@
-const botconfig = require("./botconfig.json");
-const token = require("./token.json");
-const Discord = require("discord.js");
-const mysql = require("mysql");
+require('dotenv').config();
+const Discord = require('discord.js')
+//import Sequelize, { Model as _Model, TEXT, TINYINT } from 'sequelize';
+const Sequelize = require('sequelize');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+const dbUser = process.env.USER;
+const dbPass = process.env.PASS;
+const dbName = process.env.DBNAME;
+const dbHost = process.env.HOST
 
-const bot = new Discord.Client({disableEveryone: true})
-const prefix = botconfig.prefix;
-
-bot.on("ready", async()=>{
-
-console.log(`${bot.user.username} is online!`);
-bot.user.setActivity("D&D 5E")
+//establish db connection
+const sequelize = new Sequelize(dbName, dbUser, dbPass, {
+  host: dbHost,
+  dialect: 'mysql'
 });
 
-bot.on('message', async message => {
+//test db connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
-
-
-    const args = message.content.slice(prefix.length).split(/ */)
-    const cmd = args.shift().toLowerCase();
-
-        console.log(`command: ${cmd}\nArgs: ${args}`)
-
-    if(cmd === `${prefix}getinfo`){
-        let botembed = new Discord.RichEmbed()
-        .setDescription("Justin is god, bow to him")
-        .setColor("#15f153")
-        .addField("Bot Name", bot.user.username);
-
-        return message.channel.send(botembed);
-    }
+//define movie model
+const Model = Sequelize.Model;
+class Movie extends Model { }
+Movie.init({
+  // attributes
+  name: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  played: {
+    type: Sequelize.TINYINT,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: 'movie'
+  // options
 });
 
-bot.login(token.token);
+//log in to discord
+bot.login(TOKEN);
+
+//confirm bot is loaded
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+
+  //Look for suggest prefix
+  if (msg.content.startsWith('!suggest')) {
+
+    //remove prefix from message to isolate movie name
+    let name = msg.content.replace('!suggest ', '');
+
+    //create db entry
+    Movie.create({ name: name, played: 0 });
+
+    //alert user that suggestion has been added to list
+    msg.channel.send(name + ' has been added to the suggestion list.');
+
+  }
+
+  //look for list prefix
+  if (msg.content.startsWith('!list')) {
+
+    //gather movie list and then format list.
+    list_callback().then(function (result) {
+
+      let messagestring = '```'
+let messageArray = [];
+      console.log(result[0].id);
+      for (i = 0; i < result.length; i++){
+
+
+        messageArray[i] = result[i].id + '. ' + result[i].name;
+
+        
+      }
+
+      //display list     
+
+      messagestring = '```Movie List \n\n'+messageArray.join('\n')+'```';
+      msg.channel.send(messagestring);
+    })
+  }
+  if (msg.content.startsWith('!remove')) {
+
+    let id = msg.content.replace('!remove ', '');
+
+    const movieToRemove = remove_callback(id).then(function (result) {
+
+      msg.channel.send('Removed!');
+    });
+
+  }
+
+  if (msg.content.startsWith('!newpoll')) {
+
+    var names = [];
+    const thisWeeksMovies = poll_callback().then(function (result) {
+
+
+      bot.channels.get(`699741363436257310`).send('/poll "Movie Poll" ' +'"'+result[0].name+'"'+'"'+result[1].name+'"'+'"'+result[2].name+'"'+'"'+result[3].name+'"')
+    });
+
+  }
+
+});
+
+function get_all_movies() {
+
+  return new Promise(function (resolve, reject) {
+    resolve(Movie.findAll({
+
+      where: {
+        played: 0
+      }
+    }));
+  }
+
+  )
+};
+
+function get_movie_by_pk(id) {
+
+  return new Promise(function (resolve, reject) {
+    resolve(Movie.findByPk(id).then(function (result) {
+      if (result) {
+        result.update({
+          played: 1
+        })
+      }
+    }));
+  })
+};
+
+async function list_callback() {
+  let getmovies = await get_all_movies();
+
+  return getmovies;
+}
+async function remove_callback(id) {
+
+  let movie_to_remove = await get_movie_by_pk(id);
+
+  return movie_to_remove;
+}
+
+function create_poll() {
+
+  return new Promise(function (resolve, reject) {
+    resolve(Movie.findAll({
+      where: {
+        played:0
+      },
+    
+      order: sequelize.random(),
+      limit: 4
+    }))
+  });
+}
+async function poll_callback() {
+  let getpoll = await create_poll();
+
+  return getpoll;
+}
